998611557e5ab84beb35b202f7c0231bd5beb96c
HyeW <qorgp13@naver.com>
2022-05-17 14:58:54 +0900

    Create 1339 단어 수학

---
+++ BOJ/Greedy/1339_단어_수학/HyeW/README.md
@@ -0,0 +1,13 @@
+## Info
+
+<a href="https://www.acmicpc.net/problem/1339" rel="nofollow">1339 단어 수학</a>
+
+## ❗ 풀이
+
+
+
+## ❗ 추가 지식
+
+
+## 🙂 마무리
+
---
+++ BOJ/Greedy/1339_단어_수학/HyeW/wordMath.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class wordMath {
+    static char[][] str;
+    static PriorityQueue<Integer> arrsum = new PriorityQueue<>(Collections.reverseOrder());
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int ans = 0;
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        str = new char[n][];
+        int strlen;
+
+        for(int i = 0; i < n; i++) {
+            int count;
+            str[i] = br.readLine().toCharArray();
+            strlen = str[i].length;
+
+            count = strlen -1;
+            for(int j = 0; j <strlen; j++) {
+                map.put(str[i][j], map.getOrDefault(str[i][j], 0)+(int) Math.pow(10, count));
+                count--;
+            }
+        }
+
+        //HashMap 값을 꺼내 maxheap에 넣어준다.
+        Iterator<Character> it = map.keySet().iterator();
+
+        while(it.hasNext()) {
+            char c = it.next();
+            arrsum.add(map.get(c));
+        }
+
+        int i = 9;
+        while(!arrsum.isEmpty()) {
+            int value = arrsum.poll();
+            System.out.println(value);
+            ans += value*i;
+            i--;
+        }
+        System.out.println(ans);
+    }
+
+
+}
