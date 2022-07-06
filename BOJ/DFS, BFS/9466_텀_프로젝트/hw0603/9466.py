234e92fa79194ea0f0481515acfd933283318083
HyungUk Ryu <hw0603@naver.com>
2022-07-06 23:35:38 +0900

    Create 9466 텀 프로젝트

--- BOJ/DFS, BFS/9466_텀_프로젝트/hw0603/9466.py
+++ BOJ/DFS, BFS/9466_텀_프로젝트/hw0603/9466.py
@@ -9,16 +9,19 @@ for _ in range(T):
     checkResult = {}  # 해당 노드가 팀에 포함될 수 있는지 조사한 결과를 저장
 
     # 학생들 한 명 씩을 startnode로 보고 조사
+    count = n
     for node in range(1, n+1):
         history = []  # DFS에서 사용할 노드 히스토리
+        hisdict = {}  # 히스토리 리스트에서 in 연산 하면 너무 느림.. 해싱해서 있는지 없는지 판단하기 위해 딕셔너리 사용
         if (checkResult.get(node) is not None):
             continue  # 이미 조사한 결과가 있을 경우 skip
+        history.append(node)
+        hisdict[node] = True
 
         # DFS 탐색 진행
         while (True):
             node = pick[node]  # 다음 노드 선택
             if (checkResult.get(node) is not None):
-                ...
                 """
                 예전에 조사된 바 있는 노드가 들어올 경우 더 이상 탐색할 필요 없음.
                 기 조사된 노드가 이미 팀을 이루고 있는 경우 -> 어떤 경로로 그 노드에 도달했든 앞에 있는 노드로는 팀 구성 불가능
@@ -28,18 +31,20 @@ for _ in range(T):
                     checkResult[s] = False
                 break
 
-            if (node in history):
+            if (hisdict.get(node)):
                 """
                 history에 있는 노드가 다시 들어올 경우 사이클이 형성된 것 -> 현재 탐색된 노드 중 팀 구성 가능한 노드 존재
                 - node 이전에 들어온(인덱스가 더 작은) 노드들은 모두 팀 구성 불가 -> checkResult=False
                 - node를 포함하여 그 이후에 들어온 노드들끼리 팀 구성 가능 -> checkResult=True
                 """
                 idx = history.index(node)
-                for i in range(1, len(history)+1):
-                    checkResult[i] = False if i < idx else True
+                for i in range(len(history)):
+                    checkResult[history[i]] = False if i < idx else True
+                    if (checkResult[history[i]]):
+                        count -= 1
+                break
                 
             history.append(node)
-    print(checkResult)
+            hisdict[node] = True
 
-
-
+    print(count)
